721ccd39a1077233c76b41958ee2902aab26bb01
ynx.aron <ynx.aron@outlook.com>
2025-07-24 07:09:43 +0530

    Updated With Bonus Exercise

--- Solutions/S1/S/mod.svelte
+++ Solutions/S1/S/mod.svelte
@@ -1,20 +1,25 @@
 <script>
     let name = $state('');
     let tone_formal = $state(false);
+    let all_shown = $state(true);
 </script>
 
 <button onclick={() => tone_formal = !tone_formal}>Toggle Formal</button>
 <input bind:value={name} placeholder="Enter Name">
-{#if name}
-    {#if tone_formal}
-        <p>Grettings, {name}</p>
-    {:else}
-        <p>Hi, {name}</p>
+{#if all_shown}
+    {#if name}
+        {#if tone_formal}
+            <p>Grettings, {name}</p>
+        {:else}
+            <p>Hi, {name}</p>
     {/if}
-{:else}
-    {#if tone_formal}
-        <p>Welcome!</p>
     {:else}
-        <p>Hi There!</p>
+        {#if tone_formal}
+            <p>Welcome!</p>
+        {:else}
+            <p>Hi There!</p>
+        {/if}
     {/if}
 {/if}
+
+<button onclick={() => all_shown = !all_shown}>Hide/Show All</button>
